e3b4dc6f2a67ed10ccf6e4c18b4450839f842294
Lagit <83742377+Lagit-star@users.noreply.github.com>
2021-09-18 22:15:07 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/user/bin/bash
+
+clear
+
+figlet .......LAGIT....... | lolcat
+
+
+
+echo 'INSTALLING YOUR COMPULSORY PACKASES FROM TOR SO BE.'.| lolcat 
+
+sleep 5
+clear 
+echo 'INSTALLING PYTHON...❗❗SO BE PATIENCE!!' | lolcat 
+sleep 4
+pkg install python
+pkg install python2
+clear 
+echo 'INSTALLING PYTTSX3 - MODULE...❗❗SO BE PATIENCE!!' | lolcat
+sleep 4
+
+pip install pyttsx3
+clear 
+sleep 4
+echo 'INSTALLING PY-FIGLET...❗❗SO BE PATIENCE!!' | lolcat
+sleep 4
+pip install pyfiglet
+clear 
+echo 'INSTALLING GIT...❗❗SO BE PATIENCE!!' | lolcat
+sleep 4
+pkg install git
+clear 
+pkg  install wget
+clear 
+apt install nmap
+clear 
+pkg install termux-api
+clear 
+
+sleep 5
+
+figlet .......LAGIT....... | lolcat
+
+echo 'SO ALL COMPULSURY PACKAGES ARE UPTO DATE -!!' | lolcat 
+
